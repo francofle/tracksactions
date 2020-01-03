b3a129084641fc3771768f47a128059cd96a1949
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2020-01-03 13:11:41 -0500

    Created Transactions component. made it to take the rest of the viewport

---
+++ client/src/Components/TransactionsTable/transactionsTable.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './transactionsTable.styles.sass';
+
+const TransactionsTable = () => {
+  return (
+    <div className='transactionsTableContainer'>
+      <div className="transactionsTableDiv">
+        <h1>Transactions</h1>
+      </div>
+    </div>
+  )
+};
+
+export default TransactionsTable;
--- client/src/Pages/HomePage/homepage.component.jsx
+++ client/src/Pages/HomePage/homepage.component.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
+import './homepage.styles.sass'
 import { connect } from 'react-redux';
 import AccountSummary from "../../Components/AccountSummary/accountSummary.component";
 import {createStructuredSelector} from "reselect";
 import {selectCurrentUser} from "../../redux/user/user.selectors";
+import TransactionsTable from "../../Components/TransactionsTable/transactionsTable.component";
 
 const HomePage = ({currentUser}) => {
   return (
-    <div>
+    <div className='homepage'>
       <AccountSummary currentUser={currentUser}/>
+      <TransactionsTable />
     </div>
   )
 };
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
-})
+});
 
 
 export default connect(mapStateToProps)(HomePage);
