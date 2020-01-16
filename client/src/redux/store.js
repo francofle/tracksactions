d74b022d2f29fd236a6b87c1991d6e3cbc75dfff
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2020-01-16 17:04:18 -0500

    changed logger middleware to load only on 'development' env

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,7 +2,11 @@ import { createStore, applyMiddleware } from 'redux';
 import { logger } from 'redux-logger';
 import rootReducer from './rootReducer';
 
-const middleWares = [logger];
+const middleWares = [];
+
+if (process.env.NODE_ENV === 'development') {
+  middleWares.push(logger);
+}
 
 const store = createStore(rootReducer, applyMiddleware(...middleWares));
 
