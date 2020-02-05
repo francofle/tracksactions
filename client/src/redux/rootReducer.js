a2b57021df5e94b0e3fff64e5edd192da5b17f46
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2020-02-05 00:50:29 -0500

    format files.

--- client/src/redux/rootReducer.js
+++ client/src/redux/rootReducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import userReducer from './user/user.reducer';
-import transactionReducer from './transaction/transcation.reducer'
+import transactionReducer from './transaction/transcation.reducer';
 
 export default combineReducers({
   user: userReducer,
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import { logger } from 'redux-logger';
 import rootReducer from './rootReducer';
-import thunk from "redux-thunk";
+import thunk from 'redux-thunk';
 
 const middleWares = [thunk];
 
